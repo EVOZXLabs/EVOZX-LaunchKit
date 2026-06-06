5347cf3ebddb5a2ef29409a58dbc1069372d9e3d
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-06-06 10:25:21 +0700

    Refactor CONFIG object formatting for consistency

--- js/config.js
+++ js/config.js
@@ -1,42 +1,42 @@
 const CONFIG = {
 
-/* FACTORY */
+    /* FACTORY */
 
-FACTORY_ADDRESS:
-    "0x3F810a44D29a4f0fF7880641E69EBCBc076dA220",
+    FACTORY_ADDRESS:
+        "0x3F810a44D29a4f0fF7880641E69EBCBc076dA220",
 
-/* EVOZX TOKEN */
+    /* EVOZX TOKEN */
 
-EVOZX_ADDRESS:
-    "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
+    EVOZX_ADDRESS:
+        "0x032a962F62Fc1cbc15B19767Aa138deA3B454B74",
 
-/* EVOZX EXCHANGE */
+    /* EVOZX EXCHANGE */
 
-EXCHANGE_ADDRESS:
-    "0x24cCb720F7F8b9247FB50A88F6A6a5A5DD7d9ab8",
+    EXCHANGE_ADDRESS:
+        "0x24cCb720F7F8b9247FB50A88F6A6a5A5DD7d9ab8",
 
-TREASURY_ADDRESS:
-    "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C",
+    TREASURY_ADDRESS:
+        "0x50Cd30Ff7f0fbBD9d0FDe1F60DE8c52D6F390c5C",
 
-EXCHANGE_RATE:
-    5,
+    EXCHANGE_RATE:
+        5,
 
-/* NETWORK */
+    /* NETWORK */
 
-CHAIN_ID:
-    805,
+    CHAIN_ID:
+        805,
 
-CHAIN_NAME:
-    "EVOZ Mainnet",
+    CHAIN_NAME:
+        "EVOZ Mainnet",
 
-RPC_URL:
-    "https://rpc.evozscan.com",
+    RPC_URL:
+        "https://rpc.evozscan.com",
 
-EXPLORER_URL:
-    "https://evozscan.com",
+    EXPLORER_URL:
+        "https://evozscan.com",
 
-CURRENCY_SYMBOL:
-    "EVOZ"
+    CURRENCY_SYMBOL:
+        "EVOZ"
 
 };
 
@@ -44,132 +44,132 @@ CURRENCY_SYMBOL:
 
 const FACTORY_ABI = [
 
-{
-    "anonymous": false,
-    "inputs": [
-        {
-            "indexed": true,
-            "internalType": "address",
-            "name": "creator",
-            "type": "address"
-        },
-        {
-            "indexed": true,
-            "internalType": "address",
-            "name": "token",
-            "type": "address"
-        },
-        {
-            "indexed": false,
-            "internalType": "string",
-            "name": "name",
-            "type": "string"
-        },
-        {
-            "indexed": false,
-            "internalType": "string",
-            "name": "symbol",
-            "type": "string"
-        },
-        {
-            "indexed": false,
-            "internalType": "uint256",
-            "name": "supply",
-            "type": "uint256"
-        }
-    ],
-    "name": "TokenCreated",
-    "type": "event"
-},
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "creator",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "token",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "symbol",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "supply",
+                "type": "uint256"
+            }
+        ],
+        "name": "TokenCreated",
+        "type": "event"
+    },
 
-{
-    "inputs": [
-        {
-            "internalType": "string",
-            "name": "name_",
-            "type": "string"
-        },
-        {
-            "internalType": "string",
-            "name": "symbol_",
-            "type": "string"
-        },
-        {
-            "internalType": "uint256",
-            "name": "supply_",
-            "type": "uint256"
-        }
-    ],
-    "name": "createToken",
-    "outputs": [
-        {
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-},
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "name_",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "symbol_",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "supply_",
+                "type": "uint256"
+            }
+        ],
+        "name": "createToken",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
 
-{
-    "inputs": [
-        {
-            "internalType": "uint256",
-            "name": "index",
-            "type": "uint256"
-        }
-    ],
-    "name": "getToken",
-    "outputs": [
-        {
-            "internalType": "address",
-            "name": "token",
-            "type": "address"
-        },
-        {
-            "internalType": "address",
-            "name": "creator",
-            "type": "address"
-        },
-        {
-            "internalType": "string",
-            "name": "name",
-            "type": "string"
-        },
-        {
-            "internalType": "string",
-            "name": "symbol",
-            "type": "string"
-        },
-        {
-            "internalType": "uint256",
-            "name": "supply",
-            "type": "uint256"
-        },
-        {
-            "internalType": "uint256",
-            "name": "createdAt",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-},
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "index",
+                "type": "uint256"
+            }
+        ],
+        "name": "getToken",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "token",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "creator",
+                "type": "address"
+            },
+            {
+                "internalType": "string",
+                "name": "name",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "symbol",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "supply",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "createdAt",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
 
-{
-    "inputs": [],
-    "name": "totalTokens",
-    "outputs": [
-        {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-}
+    {
+        "inputs": [],
+        "name": "totalTokens",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 
 ];
 
@@ -177,55 +177,55 @@ const FACTORY_ABI = [
 
 const EXCHANGE_ABI = [
 
-{
-    "inputs": [],
-    "name": "buyEVOZX",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-},
+    {
+        "inputs": [],
+        "name": "buyEVOZX",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
 
-{
-    "inputs": [],
-    "name": "getAvailableStock",
-    "outputs": [
-        {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-},
+    {
+        "inputs": [],
+        "name": "getAvailableStock",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
 
-{
-    "inputs": [],
-    "name": "rate",
-    "outputs": [
-        {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-},
+    {
+        "inputs": [],
+        "name": "rate",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
 
-{
-    "inputs": [],
-    "name": "paused",
-    "outputs": [
-        {
-            "internalType": "bool",
-            "name": "",
-            "type": "bool"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-}
+    {
+        "inputs": [],
+        "name": "paused",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 
 ];
 
@@ -233,38 +233,38 @@ const EXCHANGE_ABI = [
 
 const ERC20_ABI = [
 
-{
-    "inputs": [
-        {
-            "internalType": "address",
-            "name": "account",
-            "type": "address"
-        }
-    ],
-    "name": "balanceOf",
-    "outputs": [
-        {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-},
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
 
-{
-    "inputs": [],
-    "name": "decimals",
-    "outputs": [
-        {
-            "internalType": "uint8",
-            "name": "",
-            "type": "uint8"
-        }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-}
+    {
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
 
 ];
